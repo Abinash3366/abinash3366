1e8cbdbe6c6ff4fb8c8f4573832b06a17e0dfd56
Abinash Ram Sunthar <Abinash3366@gmail.com>
2021-10-19 17:53:15 +1100

    Delete 404 directory

--- 404/assets/script.js
+++
@@ -1,5 +0,0 @@
-let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if(matched)
-     document.getElementById("style").setAttribute("href", "assets/dark_style.css");
-else
-    console.log('Currently not in dark mode');
